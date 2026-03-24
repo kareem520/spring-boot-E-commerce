c4423084b9199e86d03f6edbbc7aa29459255ae2
Karim Fathi <karimfathi542@gmail.com>
2026-03-24 21:30:59 +0200

    test4

--- spring-ecomm/src/main/java/com/ecomm/spring_ecomm/SpringEcommApplication.java
+++ spring-ecomm/src/main/java/com/ecomm/spring_ecomm/SpringEcommApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringEcommApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringEcommApplication.class, args);;
+		SpringApplication.run(SpringEcommApplication.class, args);
 	}
 }
